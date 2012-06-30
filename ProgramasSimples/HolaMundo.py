a9ce457cfa1bfa30ddb9caa42d88e31d821643b4
Alevardi <Amet.Alvirde@gmail.com>
2012-06-30 00:23:06 -0500

    Hola mundo.
    "Hola mundo explicado en comentarios"

---
+++ ProgramasSimples/HolaMundo.py
@@ -0,0 +1,15 @@
+# Nota: Los comentarios no estan acentuados debido 
+# a que Python puede tener problemas con ellos
+# en algunas computadoras. Si quieres que tu codigo
+# funcione en cualquier computadora, no los uses.
+
+# Definicion del metodo main.
+def main():
+ 	print "Hola, mundo" 
+
+# Este bloque de codigo ira al pie de cualquier
+# programa escrito en python Se utiliza para
+# llamar el metodo main e iniciar el programa.
+
+if __name__ == '__main__':
+  main()
