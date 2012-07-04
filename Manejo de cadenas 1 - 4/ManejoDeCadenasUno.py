748c1e951d50c7703cea6f84b67e5c214214f9d2
Alevardi <Amet.Alvirde@gmail.com>
2012-07-03 23:29:28 -0500

    Añadido el código de "Manejo de cadenas 1/4"

---
+++ Manejo de cadenas 1 - 4/ManejoDeCadenasUno.py
@@ -0,0 +1,23 @@
+def main():
+	
+	sujeto = "Python"
+	predicado = 'Estoy programando con'
+	print predicado + sujeto
+	print predicado, sujeto	
+	comillasCombinadas = "Estoy escuchando 'The four horsemen'"
+	print comillasCombinadas
+	comillasCombinadasAlReves = 'Estoy escuchando "The mechanix"'
+	print comillasCombinadasAlReves
+	doblesComillasDobles = " Esto es \"inception\" de comillas dobles "
+	print doblesComillasDobles
+	doblesComillasSimples = 'Esto es \'Inception\' de comillas simples'
+	print doblesComillasSimples
+	secuenciasDeEscape = " Salto de linea\n tabulacion\t tabulacion vertical\v"
+	print secuenciasDeEscape
+	cadenaLarga = "Esta es una cadena larga \n y ocupa dos renglones"
+	print cadenaLarga
+	cadenaYTabulacion = "mi edad es: \t 17"
+	print cadenaYTabulacion
+
+if __name__ == '__main__':
+	main()
