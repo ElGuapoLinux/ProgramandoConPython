ff8a4934aa4d950fd711915ec2a6060f72216c0c
Amet <Amet.Alvirde@gmail.com>
2012-07-16 23:24:47 -0500

    Update master

--- Manejo de números/numerosTres.py
+++ Manejo de números/numerosTres.py
@@ -7,11 +7,11 @@ def main():
   c = 98
   d = 36.4
 
-  #print "el valor más bajo es: \t", floor(d) # floor = 'piso'
-  #print "el valor más alto es \t", ceil(d)    #ceiling = 'techo'
-  #print "Éste es el número pi: \t", pi
-  #print "Logaritmo base 10 de 10: \t", log10(10) #sintaxis log10(x)
-  #print "Logaritmo natural (o neperiano) de e: \t", log(e)
+  print "el valor más bajo es: \t", floor(d) # floor = 'piso'
+  print "el valor más alto es \t", ceil(d)    #ceiling = 'techo'
+  print "Éste es el número pi: \t", pi
+  print "Logaritmo base 10 de 10: \t", log10(10) #sintaxis log10(x)
+  print "Logaritmo natural (o neperiano) de e: \t", log(e)
 
   print "El número más grande de mis variables es: \t", max(a,b,c,d,1000) #max(a,b,9)  
   print "El número más pequeño de mis variables es: \t", min(a,b,c,d)     #min(a,b,9)
